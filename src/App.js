a7cb3ea9e1814b38efb337768d3711680ac75e30
offiahchiaz <offiahchiaz@gmail.com>
2022-03-23 23:25:35 +0100

    Prevent entire App tree from displaying until auth is ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useAuthContext } from './hooks/useAuthContext';
+
 import './App.css';
 
 function App() {
+  const { authIsReady } = useAuthContext()
+
   return (
     <div className="App">
-      
+      { authIsReady && (
+        <h1>lol</h1>
+      )}
     </div>
   );
 }
