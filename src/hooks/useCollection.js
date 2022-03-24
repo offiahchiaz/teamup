c3e18c67f65f03e70bc5e4572d62ad396313efc5
offiahchiaz <offiahchiaz@gmail.com>
2022-03-24 03:34:00 +0100

    Added an optional where cluase for fetching loggedin user documents

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,12 +1,18 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { projectFirestore } from "../firebase/config"
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query) => {
   const [documents, setDocuments] = useState(null)
   const [error, setError] = useState(null)
 
+  const query = useRef(_query).current
+
   useEffect(() => {
     let ref = projectFirestore.collection(collection)
+
+    if (query) {
+      ref = ref.where(...query)
+    }
 
     const unsubscribe = ref.onSnapshot((snapshot) => {
       let results = []
@@ -25,7 +31,7 @@ export const useCollection = (collection) => {
     // unsubscribe on unmount ie, return a cleanup function
     return () => unsubscribe()
 
-  }, [collection])
+  }, [collection, query])
 
   return { documents, error }
 }
