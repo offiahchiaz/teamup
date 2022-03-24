ea56e526400b832780e41cc6f0179c571098b95e
offiahchiaz <offiahchiaz@gmail.com>
2022-03-24 03:42:52 +0100

    Added optional orderBy query for displaying docs in either ascending or descending order

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,17 +1,22 @@
 import { useEffect, useRef, useState } from "react"
 import { projectFirestore } from "../firebase/config"
 
-export const useCollection = (collection, _query) => {
+export const useCollection = (collection, _query, _orderBy) => {
   const [documents, setDocuments] = useState(null)
   const [error, setError] = useState(null)
 
   const query = useRef(_query).current
+  const orderBy = useRef(_orderBy).current
 
   useEffect(() => {
     let ref = projectFirestore.collection(collection)
 
     if (query) {
       ref = ref.where(...query)
+    }
+
+    if (orderBy) {
+      ref = ref.orderBy(...orderBy)
     }
 
     const unsubscribe = ref.onSnapshot((snapshot) => {
@@ -31,7 +36,7 @@ export const useCollection = (collection, _query) => {
     // unsubscribe on unmount ie, return a cleanup function
     return () => unsubscribe()
 
-  }, [collection, query])
+  }, [collection, query, orderBy])
 
   return { documents, error }
 }
