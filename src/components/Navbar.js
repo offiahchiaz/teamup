06cbc9029d4419deb8007db06be4e52210730da3
offiahchiaz <offiahchiaz@gmail.com>
2022-03-26 02:36:53 +0100

    Conditionally display Navbar links based on user signed in or not

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 import './Navbar.css'
 import Temple from '../assets/temple.svg'
@@ -7,6 +8,8 @@ import Temple from '../assets/temple.svg'
 
 export default function Navbar() {
   const { logout, isPending } = useLogout()
+  const { user } = useAuthContext()
+
   return (
     <div className='navbar'>
       <ul>
@@ -15,12 +18,20 @@ export default function Navbar() {
           <span>TeamUp</span>
         </li>
 
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="signup">Signup</Link></li>
-        <li>
-          {!isPending && <button className='btn' onClick={logout}>Logout</button>}
-          {isPending && <button className='btn' disabled>Logging out...</button>}
-        </li>
+        {!user && (
+         <>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="signup">Signup</Link></li>
+         </>
+        )}
+
+        {user && ( 
+          <li>
+            {!isPending && <button className='btn' onClick={logout}>Logout</button>}
+            {isPending && <button className='btn' disabled>Logging out...</button>}
+          </li>
+        )}
+        
       </ul>
     </div>
   )
