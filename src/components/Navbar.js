5cb8f8fc75377bd3172805d94d625abb69097724
offiahchiaz <offiahchiaz@gmail.com>
2022-03-26 01:01:30 +0100

    User can signout and online parameter turned false once signed out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 import './Navbar.css'
 import Temple from '../assets/temple.svg'
 
 
 export default function Navbar() {
+  const { logout, isPending } = useLogout()
   return (
     <div className='navbar'>
       <ul>
@@ -15,7 +17,10 @@ export default function Navbar() {
 
         <li><Link to="/login">Login</Link></li>
         <li><Link to="signup">Signup</Link></li>
-        <li><button className='btn'>Logout</button></li>
+        <li>
+          {!isPending && <button className='btn' onClick={logout}>Logout</button>}
+          {isPending && <button className='btn' disabled>Logging out...</button>}
+        </li>
       </ul>
     </div>
   )
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
-import { projectAuth } from "../firebase/config";
+import { projectAuth, projectFirestore } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useLogout = () => {
   const [isCancelled, setIsCancelled] = useState(false)
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useAuthContext()
+  const { user, dispatch } = useAuthContext()
 
   const logout = async () => {
     setError(null)
@@ -14,6 +14,10 @@ export const useLogout = () => {
 
     // sign the user out
     try {
+      // update online status
+      const { uid } = user
+      await projectFirestore.collection('users').doc(uid).update({ online: false })
+      
       await projectAuth.signOut()
 
       // dispatch logout action
