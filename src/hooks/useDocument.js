e3d5ab8578d333fbdd0a88d325d433a186c760e5
offiahchiaz <offiahchiaz@gmail.com>
2022-04-01 02:03:48 +0100

    Addded useDocument hook for getting details on a specific document

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import { projectFirestore } from "../firebase/config"
+
+
+export const useDocument = (collection, id) => {
+  const [ document, setDocument] = useState(null)
+  const [error, setError] = useState(null)
+
+  // realtime data for document
+  useEffect(() => {
+    const ref = projectFirestore.collection(collection).doc(id)
+
+    const unsubscribe = ref.onSnapshot((snapshot) => {
+      setDocument({ ...snapshot.data(), id: snapshot.id })
+      setError(null)
+    }, (err) => {
+      console.log(err.message)
+      setError('Failed to get document')
+    })
+
+    // return a cleanup function
+    return () => unsubscribe()
+
+  }, [collection, id])
+
+  return { document, error }
+}
