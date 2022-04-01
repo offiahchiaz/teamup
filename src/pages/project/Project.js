3b43440c0dbcb9e5680de61043fa315a9d48c2df
offiahchiaz <offiahchiaz@gmail.com>
2022-04-01 02:46:46 +0100

    Updated Project details with useDocument hooks for displaying project detials

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,11 +1,24 @@
+import { useParams } from 'react-router-dom'
+import { useDocument } from '../../hooks/useDocument'
 
 import './Project.css'
 
-import React from 'react'
 
 export default function Project() {
+  const { id } = useParams()
+  const { document, error } = useDocument('projects', id)
+
+  if (error) {
+    return <div className="error">{error}</div>
+  }
+  if (!document) {
+    return <div className="loading">Loading...</div>
+  }
+
   return (
-    <div>Project</div>
+    <div className='project-details'>
+      <h1>{ document.name }</h1>
+    </div>
   )
 }
 
