73533269d74a016c3f4ade34976b80cbc9220f4a
offiahchiaz <offiahchiaz@gmail.com>
2022-04-03 04:57:10 +0100

    Updated Dashboard with filtering component design

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,7 +1,9 @@
 import ProjectList from '../../components/ProjectList'
 import { useCollection } from '../../hooks/useCollection'
+import ProjectFilter from './ProjectFilter'
 
 import './Dashboard.css'
+
 
 export default function Dashboard() {
   const { documents, error } = useCollection('projects')
@@ -10,6 +12,7 @@ export default function Dashboard() {
     <div>
       <h2 className='page-title'>Dashboard</h2>
       {error && <p className='error'>{error}</p> }
+      {documents && <ProjectFilter />}
       { documents && <ProjectList projects={documents} /> }
     </div>
   )
